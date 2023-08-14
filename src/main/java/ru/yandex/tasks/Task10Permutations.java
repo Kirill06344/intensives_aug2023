e4d9471055008ed59145f773d0f74b2ce63a434f
stewie <kirill12093@mail.ru>
2023-08-14 20:56:41 +0300

    feat: task10

--- src/main/java/ru/yandex/tasks/Task10Permutations.java
+++ src/main/java/ru/yandex/tasks/Task10Permutations.java
@@ -1,15 +1,49 @@
 package ru.yandex.tasks;
 
-import java.util.Arrays;
+import javax.swing.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Task10Permutations {
     public static int[][] permutations(int[] numbers) {
-        /*
-         * Возвращает массив со всеми перестановками массива numbers (до 10 элементов, все по модулю до 10^5)
-         */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        var resultInList = perms(Arrays.stream(numbers)
+                .boxed()
+                .collect(Collectors.toList()));
+
+        int [][] result = new int[resultInList.size()][];
+        for (int i = 0; i < resultInList.size(); ++i) {
+            result[i] = resultInList.get(i).stream().mapToInt(el -> el).toArray();
+        }
+        return result;
     }
+
+
+    public static List<List<Integer>> perms(List<Integer> numbers) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (numbers.size() == 2) {
+            List<Integer> firstPerm = new ArrayList<>();
+            firstPerm.add(numbers.get(0));
+            firstPerm.add(numbers.get(1));
+            List<Integer> secondPerm = new ArrayList<>();
+            secondPerm.add(numbers.get(1));
+            secondPerm.add(numbers.get(0));
+            res.add(firstPerm);
+            res.add(secondPerm);
+            return res;
+        }
+        for (int i = 0; i < numbers.size(); ++i) {
+            List<Integer> copyWithoutElement = new ArrayList<>();
+            copyWithoutElement.addAll(numbers);
+            copyWithoutElement.remove(i);
+            var recurseResult = perms(copyWithoutElement);
+            for (var l : recurseResult) {
+                l.add(0, numbers.get(i));
+            }
+            res.addAll(recurseResult);
+        }
+        return res;
+    }
+
 
     public static void selfCheck() {
         int[] input = {1, 2, 3};
@@ -23,5 +57,9 @@ public class Task10Permutations {
         };
 
         assert Arrays.deepEquals(output, permutations(input));
+    }
+
+    public static void main(String[] args) {
+        selfCheck();
     }
 }
