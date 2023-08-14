3b0870100a9820960b13415ea94f5e9b3b2cc539
stewie <kirill12093@mail.ru>
2023-08-14 19:42:35 +0300

    feat: task7

--- src/main/java/ru/yandex/tasks/Task7PrimeNumbers.java
+++ src/main/java/ru/yandex/tasks/Task7PrimeNumbers.java
@@ -1,20 +1,33 @@
 package ru.yandex.tasks;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Task7PrimeNumbers {
-    public static int[] findPrimes (int N) {
-        /*
-         * 2 <= N <= 10^6
-         * Выход: отсортированный массив всех простых чисел от 2 до N
-         */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+    public static int[] findPrimes (int n) {
+        List<Integer> res = new ArrayList<>();
+        boolean [] primes = new boolean[n + 1];
+        for (int i = 2; i <= n; ++i) {
+            if (!primes[i]) {
+                res.add(i);
+                int j = 2;
+                while (i * j <= n) {
+                    primes[i * j] = true;
+                    j++;
+                }
+            }
+        }
+        return res.stream().mapToInt(i -> i).toArray();
     }
 
     public static void selfCheck() {
-        int[] output = {2, 3, 5};
+        int[] output = {2, 3, 5, 7, 11, 13, 17, 19, 23};
 
-        assert (Arrays.equals(output, findPrimes(5)));
+        assert (Arrays.equals(output, findPrimes(24)));
+    }
+
+    public static void main(String[] args) {
+        selfCheck();
     }
 }
