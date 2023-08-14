c6f0ba6b8949cadaf498671a2f4f9ec6d4b226a5
stewie <kirill12093@mail.ru>
2023-08-14 19:32:51 +0300

    feat: task6

--- src/main/java/ru/yandex/tasks/Task6BraceBalance.java
+++ src/main/java/ru/yandex/tasks/Task6BraceBalance.java
@@ -1,13 +1,30 @@
 package ru.yandex.tasks;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Task6BraceBalance {
     public static boolean checkBalance(String sequence) {
-        /*
-         * sequence - последовательность скобок []{}() длины до 10^5
-         * Выход: true/false, является ли строка ПСП
-         */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return true;
+        Deque<Character> stack = new LinkedList<>();
+        String leftBraces = "[({";
+        for (int i = 0; i < sequence.length(); ++i) {
+            char cur = sequence.charAt(i);
+            if (leftBraces.indexOf(cur) != -1) {
+                if (cur == '[') {
+                    stack.addFirst(']');
+                } else if (cur == '{') {
+                    stack.addFirst('}');
+                } else {
+                    stack.addFirst(')');
+                }
+            } else {
+                if (stack.isEmpty() || stack.peek() != cur) {
+                    return false;
+                }
+                stack.removeFirst();
+            }
+        }
+        return stack.isEmpty();
     }
 
     public static void selfCheck() {
@@ -16,5 +33,9 @@ public class Task6BraceBalance {
 
         assert checkBalance(test1);
         assert !checkBalance(test2);
+    }
+
+    public static void main(String[] args) {
+        selfCheck();
     }
 }
