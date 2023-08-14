3e396e528e2e1ceae14ca658a8a0e7347af7088c
stewie <kirill12093@mail.ru>
2023-08-14 18:50:25 +0300

    feat: task3

--- src/main/java/ru/yandex/tasks/Task3InsertionSort.java
+++ src/main/java/ru/yandex/tasks/Task3InsertionSort.java
@@ -5,18 +5,33 @@ import java.util.Arrays;
 
 public class Task3InsertionSort {
     public static ArrayList<Integer> sort(ArrayList<Integer> numbers) {
-        /*
-         * numbers: массив целых чисел, -10^5 <= numbers[i] <= 10^5, длина массива до 10^5
-         * Выход: отсортированный (сортировкой вставками!) numbers
-         */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        for (int i = 0; i < numbers.size(); ++i) {
+            int j = i;
+            while (j > 0 && numbers.get(j) < numbers.get(j - 1)) {
+                swap(numbers, j, j - 1);
+                j--;
+            }
+        }
+        return numbers;
     }
 
+
+
+    public static void swap(ArrayList<Integer> numbers, int i, int j) {
+        Integer tmp = numbers.get(i);
+        numbers.set(i, numbers.get(j));
+        numbers.set(j, tmp);
+    }
+
+
     public static void selfCheck() {
-        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(3, 4, 5, 2, 1));
-        ArrayList<Integer> output = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(3, 6,  4, 5, 2, 1));
+        ArrayList<Integer> output = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
 
         assert output.equals(sort(input));
+    }
+
+    public static void main(String[] args) {
+        selfCheck();
     }
 }
